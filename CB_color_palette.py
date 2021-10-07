3267407c533d6368fa27f7eb12fc133c8fb1f8e8
myutkin <maxim.yutkin@gmail.com@users.noreply.github.com>
2021-10-07 12:03:55 +0300

    Added colorblind palette

---
+++ CB_color_palette.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 11:50:39 2021
+
+@author: yutkinm
+"""
+
+colors = ['#000000', '#E69F00', '#56B4E9', 
+          '#009E73', '#0072B2', '#D55E00', '#CC79A7']
